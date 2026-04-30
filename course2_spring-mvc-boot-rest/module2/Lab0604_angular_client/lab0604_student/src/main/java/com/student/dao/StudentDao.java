84b9488e3de90e97c2d246aba69e4bd8f96c2546
m-abdelkawy <engineer_m.abdelkawy@yahoo.com>
2026-04-30 12:18:28 -0400

    add lab 0604 angular client

---
+++ course2_spring-mvc-boot-rest/module2/Lab0604_angular_client/lab0604_student/src/main/java/com/student/dao/StudentDao.java
@@ -0,0 +1,12 @@
+package com.student.dao;
+
+import com.student.core.Student;
+
+import java.util.Collection;
+
+public interface StudentDao {
+    Student getOne(long id);
+    Collection<Student> getAll();
+
+    void add(Student student);
+}
