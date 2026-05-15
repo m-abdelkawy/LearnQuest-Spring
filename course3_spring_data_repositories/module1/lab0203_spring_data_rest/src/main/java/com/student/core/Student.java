abdc4344abb58725878f8935393c7439afb82510
m-abdelkawy <engineer_m.abdelkawy@yahoo.com>
2026-05-15 07:50:55 -0400

    add lab0203_spring_data_rest

---
+++ course3_spring_data_repositories/module1/lab0203_spring_data_rest/src/main/java/com/student/core/Student.java
@@ -0,0 +1,98 @@
+package com.student.core;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.Collection;
+
+@Entity
+//@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
+public class Student {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "STUDENTID")
+    private long id;
+    @Column(name = "FIRSTNAME")
+    private String firstName;
+    @Column(name = "LASTNAME")
+    private String surname;
+    @Column(name = "DEPARTMENT")
+    private String dept;
+    @Column(name = "FEES")
+    private double fees;
+
+    @OneToMany(mappedBy = "student", cascade = CascadeType.ALL, orphanRemoval = true)
+    private Collection<Course> courses = new ArrayList<>();
+
+    protected Student() {
+        super();
+    }
+
+    public Student(String firstName, String surname, String dept, double fees) {
+        super();
+        this.firstName = firstName;
+        this.surname = surname;
+        this.dept = dept;
+        this.fees = fees;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getDept() {
+        return dept;
+    }
+
+    public void setDept(String dept) {
+        this.dept = dept;
+    }
+
+    public double getFees() {
+        return fees;
+    }
+
+    public void setFees(double fees) {
+        this.fees = fees;
+    }
+
+    @Override
+    public String toString(){
+        return "Student{" +
+                "id=" + id +
+                ", firstName=" + firstName +
+                ", surname=" + surname +
+                ", dept=" + dept +
+                ", fees=" + fees +
+                '}';
+    }
+
+    public Collection<Course> getCourses() {
+        return courses;
+    }
+
+    public void addCourse(Course course) {
+        this.courses.add(course);
+    }
+}
---
+++ course3_spring_data_repositories/module1/lab0203_spring_data_rest/src/main/java/com/student/service/StudentServiceImpl.java
@@ -0,0 +1,32 @@
+package com.student.service;
+
+import com.student.core.Student;
+import com.student.dao.StudentRepository;
+
+import javax.inject.Inject;
+import javax.inject.Named;
+import java.util.Collection;
+
+@Named
+public class StudentServiceImpl implements StudentService {
+
+    @Inject
+    private StudentRepository studentRepository;
+
+    @Override
+    public Student get(long id) {
+        return studentRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public Collection<Student> getAllStudents() {
+        return studentRepository.findAll();
+    }
+
+    @Override
+    public Collection<Student> getStudentsByDepartment(String department) {
+        return studentRepository.getByDept(department);
+    }
+
+
+}
